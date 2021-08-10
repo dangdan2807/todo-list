a951a7dd971e4b2fabb9a3c65c3cd6f4ad975ebd
KayJuno <dangdan2807@gmail.com>
2021-08-10 23:43:06 +0700

    update features: - next / repeat when ended - active song - scroll active song into view - play song when click

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ import songsData from "../sounds/songList.js";
 const $ = document.querySelector.bind(document);
 const $S = document.querySelectorAll.bind(document);
 
+const PLAYER_STORAGE_KEY = "KAYJUNO_PLAYER";
+
 const player = $(".player");
 const cd = $(".cd");
 const heading = $("header h2");
@@ -14,17 +16,23 @@ const prevBtn = $(".btn-prev");
 const randomBtn = $(".btn-random");
 const repeatBtn = $(".btn-repeat");
 const progress = $("#progress");
+const playlist = $(".playlist");
 
 const app = {
     currentIndex: 0,
     isPlaying: false,
-    isRandom: true,
+    isRandom: false,
     isRepeat: false,
+    config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
+    setConfig: function (key, value) {
+        this.config[key] = value;
+        localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config));
+    },
     songs: songsData,
     render: function () {
-        var htmls = this.songs.map((song) => {
+        var htmls = this.songs.map((song, index) => {
             return `
-            <div class="song">
+            <div data-index="${index}" class="song ${index === this.currentIndex ? "active" : ""}">
                 <div class="thumb"
                     style="background-image: url('${song.image}')">
                 </div>
@@ -38,7 +46,7 @@ const app = {
             </div>
             `;
         });
-        $(".playlist").innerHTML = htmls.join("");
+        playlist.innerHTML = htmls.join("");
     },
     defineProperties: function () {
         Object.defineProperty(this, "currentSong", {
@@ -98,8 +106,8 @@ const app = {
 
         // song tempo changes
         audio.ontimeupdate = function () {
-            if (this.duration) {
-                const progressPercent = Math.floor((this.currentTime / this.duration) * 100);
+            if (audio.duration) {
+                const progressPercent = Math.floor((audio.currentTime / audio.duration) * 100);
                 progress.value = progressPercent;
             }
         };
@@ -116,6 +124,8 @@ const app = {
                 _this.playRandomSong();
             }
             _this.nextSong();
+            _this.render();
+            _this.scrollActiveSong();
             audio.play();
         };
 
@@ -124,21 +134,54 @@ const app = {
             if (_this.isRandom) {
                 _this.playRandomSong();
             }
+            _this.render();
             _this.prevSong();
+            _this.scrollActiveSong();
             audio.play();
         };
 
         // handle random song on/off button
-        randomBtn.onclick = function (e) {
+        randomBtn.onclick = function () {
             _this.isRandom = !_this.isRandom;
-            this.classList.toggle("active", _this.isRandom);
+            _this.setConfig("isRandom", _this.isRandom);
+            randomBtn.classList.toggle("active", _this.isRandom);
         };
 
-        // handle on/off song repeat button
-        repeatBtn.onclick = function (e) {
+        // handle repeat song
+        repeatBtn.onclick = function () {
             _this.isRepeat = !_this.isRepeat;
-            this.classList.toggle("active", _this.isRepeat);
+            _this.setConfig("isRepeat", _this.isRepeat);
+            repeatBtn.classList.toggle("active", _this.isRepeat);
         };
+
+        // handle event when next song when audio ends
+        audio.onended = function () {
+            if (_this.isRepeat) {
+                audio.play();
+            } else {
+                nextBtn.click();
+            }
+        };
+
+        // listen to playlist click event
+        playlist.onclick = function (e) {
+            const songNode = e.target.closest(".song:not(.active)");
+            const optionElement = e.target.closest("option");
+            if (songNode || !optionElement) {
+                if (songNode) {
+                    // songNode.getAttribute("data-index");
+                    _this.currentIndex = Number(songNode.dataset.index);
+                    _this.loadCurrentSong();
+                    _this.render();
+                    audio.play();
+                }
+            } else {
+            }
+        };
+    },
+    loadConfig: function () {
+        this.isRandom = this.config.isRandom;
+        this.isRepeat = this.config.isRepeat;
     },
     loadCurrentSong: function () {
         heading.innerHTML = this.currentSong.name;
@@ -160,24 +203,26 @@ const app = {
         this.loadCurrentSong();
     },
     playRandomSong: function () {
-        let newIndex = 0;
+        let newIndex;
         do {
             newIndex = Math.floor(Math.random() * this.songs.length);
         } while (newIndex === this.currentIndex);
         this.currentIndex = newIndex;
     },
-    // repeatSong: function () {
-    //     this.currentIndex--;
-    //     if (this.currentIndex <= 0) {
-    //         this.currentIndex = this.songs.length - 1;
-    //     }
-    //     this.loadCurrentSong();
-    // },
+    scrollActiveSong: function () {
+        setTimeout(function () {
+            $(".song.active").scrollIntoView({ behavior: "smooth", block: "end" });
+        }, 300);
+    },
     start: function () {
+        this.loadConfig();
         this.defineProperties();
         this.handleEvents();
         this.loadCurrentSong();
         this.render();
+
+        randomBtn.classList.toggle("active", this.isRandom);
+        repeatBtn.classList.toggle("active", this.isRepeat);
     },
 };
 
